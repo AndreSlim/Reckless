d0166c1fea012d4eeba031ed644373081ca8b2e2
Andrés Domínguez <AndreSlim7@Gmail.com>
2017-09-10 16:46:46 -0500

    Creación del menú Circular
    Se ha creado el menú circular, primero agregando las dependencias en los
    Gradle, una en cada una.
    
    Se han agregado los vectores para que se puedan dibujar, todo esto en la
    carpeta de Drawable.

--- app/src/main/java/com/andreslim/reckless/MainActivity.java
+++ app/src/main/java/com/andreslim/reckless/MainActivity.java
@@ -1,14 +1,44 @@
 package com.andreslim.reckless;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    String arreglo[] = {"arreglo de String_1", "arreglo de String_2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleMenu menuCircular = (CircleMenu) findViewById(R.id.menuCir);
+
+        menuCircular.setMainMenu(Color.parseColor("#f44336"), R.drawable.ic_nuevo_mas, R.drawable.ic_nuevo_cancelar)
+                .addSubMenu(Color.parseColor("#4CAF50"), R.drawable.ic_visitante)
+                .addSubMenu(Color.parseColor("#2196F3"), R.drawable.ic_profesor)
+                .addSubMenu(Color.parseColor("#FF5722"), R.drawable.ic_estudiante)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+
+                    @Override
+                    public void onMenuSelected(int index) {
+
+                    }
+
+                }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
+
+            @Override
+            public void onMenuOpened() {}
+
+            @Override
+            public void onMenuClosed() {}
+
+        });
 
     }
 }
