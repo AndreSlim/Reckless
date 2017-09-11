d0d45ae66293ada1fd475fdcfc69eb11951a8860
Andrés Domínguez <AndreSlim7@Gmail.com>
2017-09-10 19:05:58 -0500

    Animación del Menu circular & paleta de colores!
    Se ha creado la animación del menú circular, dentro de las
    clases del propio menú, tambien se ha colocado un if por si
    la versión de android no soporta Material Desing. Tambien
    se ha agreado un pequeño texto para especificar los botones
    y una leyenda de "alpha" para indicar la versión del
    desarrollo de software.
    
    Agregada la paleta de colores, el primario, el obscuro y
    el color de acento para cada uno de los establecidos
    para Material Desing.

--- app/src/main/java/com/andreslim/reckless/MainActivity.java
+++ app/src/main/java/com/andreslim/reckless/MainActivity.java
@@ -1,8 +1,15 @@
 package com.andreslim.reckless;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
@@ -10,19 +17,23 @@ import com.hitomi.cmlibrary.OnMenuStatusChangeListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    String arreglo[] = {"arreglo de String_1", "arreglo de String_2"};
+    TextView fondo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fondo = (TextView) findViewById(R.id.fondo_menuCir);
+
+        // - - - - - - - - - - - - < MENÚ CIRCULAR STARTS > - - - - - - - - - - - - -
+
         CircleMenu menuCircular = (CircleMenu) findViewById(R.id.menuCir);
 
         menuCircular.setMainMenu(Color.parseColor("#f44336"), R.drawable.ic_nuevo_mas, R.drawable.ic_nuevo_cancelar)
                 .addSubMenu(Color.parseColor("#4CAF50"), R.drawable.ic_visitante)
-                .addSubMenu(Color.parseColor("#2196F3"), R.drawable.ic_profesor)
-                .addSubMenu(Color.parseColor("#FF5722"), R.drawable.ic_estudiante)
+                .addSubMenu(Color.parseColor("#FF5722"), R.drawable.ic_profesor)
+                .addSubMenu(Color.parseColor("#2196F3"), R.drawable.ic_estudiante)
                 .setOnMenuSelectedListener(new OnMenuSelectedListener() {
 
                     @Override
@@ -33,12 +44,71 @@ public class MainActivity extends AppCompatActivity {
                 }).setOnMenuStatusChangeListener(new OnMenuStatusChangeListener() {
 
             @Override
-            public void onMenuOpened() {}
+            public void onMenuOpened() {
+
+                // - - - - - - - - - - - - < Efecto Revelar Starts > - - - - - - - - - - - - -
+
+                if (Build.VERSION.SDK_INT >= 21) {  //Condición para el material design
+
+                    int cx = (fondo.getLeft() + fondo.getRight()) / 2;
+                    int cy = (fondo.getTop() + fondo.getBottom()) / 2;
+
+                    // Consiguiendo el Final del radio
+                    int finalRadius = Math.max(fondo.getWidth(), fondo.getHeight());
+
+                    // Creando la animacion para esta vista, el radio de inico es cero
+                    Animator anim =
+                            ViewAnimationUtils.createCircularReveal(fondo, cx, cy, 0, finalRadius);
+
+                    // Haciendo la vista visible e iniciando la animación
+                    fondo.setVisibility(View.VISIBLE);
+                    anim.start();
+                }else{
+                    fondo.setVisibility(View.VISIBLE);  // Acción sin animación
+                }
+
+                // - - - - - - - - - - - - < Efecto Revelar Ends > - - - - - - - - - - - - -
+
+            }
 
             @Override
-            public void onMenuClosed() {}
+            public void onMenuClosed() {
 
+                // - - - - - - - - - - - - < Efecto Revelar Starts > - - - - - - - - - - - - -
+
+                if (Build.VERSION.SDK_INT >= 21) {
+
+                    // obteniendo el centro de el circulo
+                    int cx = (fondo.getLeft() + fondo.getRight()) / 2;
+                    int cy = (fondo.getTop() + fondo.getBottom()) / 2;
+
+                    // obteniendo el radio inicial de el circulo
+                    int initialRadius = fondo.getWidth();
+
+                    // creando la animación (el final del radio es cero)
+                    Animator anim =
+                            ViewAnimationUtils.createCircularReveal(fondo, cx, cy, initialRadius, 0);
+
+                    // haciendo la vista invisible cuando la animación esta acabada
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            fondo.setVisibility(View.INVISIBLE);
+                        }
+                    });
+
+                    // inicia la animación
+                    anim.start();
+                } else {
+                    fondo.setVisibility(View.INVISIBLE);  // Acción sin animación
+                }
+
+
+                // - - - - - - - - - - - - < Efecto Revelar Ends > - - - - - - - - - - - - -
+            }
         });
 
+        // - - - - - - - - - - - - < MENÚ CIRCULAR ENDS > - - - - - - - - - - - - -
     }
 }
