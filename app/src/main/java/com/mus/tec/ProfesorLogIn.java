63d498a39c1cce386f69a2773e1f19452ba137a1
Andrés Domínguez <AndreSlim7@Gmail.com>
2017-09-21 21:32:07 -0500

    Agregada Excepcion para LogIn Porfesor
    Agregada la excepción en la pantalla de LogIn del profesor
    en caso de que haya una conección inestable/nula de internet,
    o un fallo en el servidor de Firebase.
    La excepción es mostrada con un Toats.

--- app/src/main/java/com/mus/tec/ProfesorLogIn.java
+++ app/src/main/java/com/mus/tec/ProfesorLogIn.java
@@ -221,6 +221,10 @@ public class ProfesorLogIn extends AppCompatActivity{
 
                         Toast.makeText(ProfesorLogIn.this, "El correo electronico no existe o fue eliminado", Toast.LENGTH_SHORT).show();
 
+                    }else if(task.getException().getLocalizedMessage().toString().equals("A network error (such as timeout, interrupted connection or unreachable host) has occurred.")) {
+
+                        Toast.makeText(ProfesorLogIn.this, "Ha ocurrido un error de red o del servidor", Toast.LENGTH_SHORT).show();
+
                     }
 
                     Log.e("Estado Inicio Sesión","Error: "+task.getException().getLocalizedMessage());
